eb808c7a967d174177db042677088ce7fc3fd6ab
MJN <mengjingn@rd.netease.com>
2020-09-23 15:01:08 +0800

    DEV:所有方法都可以打印

--- app/src/main/java/com/gradletest/MainActivity.kt
+++ app/src/main/java/com/gradletest/MainActivity.kt
@@ -6,13 +6,41 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.e("dd", "1")
+        Log.e("log_manual", "onCreate")
         super.onCreate(savedInstanceState)
-        Log.e("dd", "9")
         setContentView(R.layout.activity_main)
+        testInit()
+        testCall("onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
     }
 
     override fun onResume() {
         super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    fun testInit(){
+        test()
+    }
+
+    fun testCall(a: String){
+    }
+
+    fun test(){
+        testCall("test")
     }
 }
